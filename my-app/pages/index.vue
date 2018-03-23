e055d2d4e8c9bc346c857b17097a3fe573f07b81
Max Huber <mahu@bluedesk.e-business.bluedesk.nl>
2018-03-23 10:34:18 +0100

    added files

--- my-app/pages/index.vue
+++ my-app/pages/index.vue
@@ -1,64 +1,36 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        my-app
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <section>
+    <div class="post-container" v-for="item in posts" :key="item.id">
+      <div class="post">
+        <h2 class="post__header text-center">{{ item.title }}</h2>
+        <div class="post__image-placeholder">
+          <img src="http://source.unsplash.com/800x500" class="post__image-placeholder--image">
+        </div>
+        <p class="post__body">{{ item.body }}</p>
+        <nuxt-link class="btn btn-default pull-right"  :to="'/details/' + item.id">Read more</nuxt-link>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Axios from 'Axios'
 
 export default {
-  components: {
-    AppLogo
-  }
+	data() {
+		return {
+			posts: []
+		}
+	},
+	created() {
+		this.fetchData()
+	},
+	methods: {
+		fetchData() {
+			Axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+				this.posts = response.data
+			})
+		}
+	}
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
